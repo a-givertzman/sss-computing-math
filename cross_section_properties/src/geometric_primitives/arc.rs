da31b19215e5c2d025c93b5c7d62f9cad5ab7c94
milovanovmaksim <milovanov160386@gmail.com>
2023-09-03 12:17:40 +0300

    implement approximate_by_line

--- cross_section_properties/src/geometric_primitives/arc.rs
+++ cross_section_properties/src/geometric_primitives/arc.rs
@@ -7,25 +7,37 @@ pub struct Arc {
     start: Point,
     terminate: Point,
     center_point: Point,
-    lines: Option<Vec<Line>>,
+    lines: Vec<Line>
 }
 
 
 impl Arc {
     pub fn new(start: Point, terminate: Point, center_point: Point) -> Self {
-        Arc { start, terminate, center_point, lines: None}
+        Arc::approximate_by_line(start, terminate, center_point)
     }
 
-    fn approximate_by_line(&self) {
+    fn approximate_by_line(start: Point, terminate: Point, center_point: Point) -> Arc {
         // Approximate an arc by lines.
+        let mut arc = Arc { start, terminate, center_point, lines: vec![]};
+        let mut lines: Vec<Line> = vec![];
         let delta = (3.0 * consts::PI) / 180.0;
-        let phi = 0.0;
-        let terminate_angle = self.terminate_angle();
-        let start_angle = self.start_angle();
+        let terminate_angle = arc.terminate_angle();
+        let mut phi = arc.start_angle();
+        let mut prev_point = arc.start;
+        let arc_radius = arc.arc_radius();
         while phi < terminate_angle && phi >= terminate_angle - delta {
-            todo!()
-
+            if phi > consts::PI * 2.0 {
+                phi = phi - consts::PI * 2.0;
+            } else { phi += delta; }
+            let x = arc.start.x + arc_radius * phi.cos();
+            let y = arc.start.y + arc_radius * phi.sin();
+            let next_point = Point::new(x, y);
+            let line = Line::new(prev_point, next_point);
+            lines.push(line);
+            prev_point = next_point;
         }
+        arc.lines = lines;
+        arc
 
     }
 
@@ -41,7 +53,7 @@ impl Arc {
     }
 
     fn arc_angle(&self) -> f64 {
-        // Arc angle
+        // Arc angle.
         let x1 = self.start.x;
         let x2 = self.terminate.x;
         let y1 = self.start.y;
@@ -51,7 +63,7 @@ impl Arc {
     }
 
     fn start_angle(&self) -> f64 {
-        // Arc start angle
+        // Arc start angle.
         let x1 = self.center_point.x + self.arc_radius();
         let x2 = self.start.x;
         let y1 = 0.0;
@@ -60,7 +72,7 @@ impl Arc {
     }
 
     fn terminate_angle(&self) -> f64 {
-        // Arc terminate  angle
+        // Arc terminate  angle.
         let x1 = self.center_point.x + self.arc_radius();
         let x2 = self.terminate.x;
         let y1 = 0.0;
@@ -74,16 +86,7 @@ impl Arc {
 impl Entity for Arc {
     fn crossed(&self, ray: &super::line::Ray) -> bool {
         let mut res = 0;
-        let lines = {
-            if let Some(ref lines) = self.lines {
-                lines
-            
-        } else {
-            self.aproximate_by_line();
-            self.lines.as_ref().unwrap()
-        }
-        };
-        for line in lines {
+        for line in self.lines.iter() {
             res += line.crossed(&ray) as u32
         }
         res > 0 && res % 2 == 1
