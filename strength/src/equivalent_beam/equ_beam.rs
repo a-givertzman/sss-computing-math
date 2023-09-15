17a6941a8aa6165ea8ea1df50c0312ced00e8795
milovanovmaksim <milovanov160386@gmail.com>
2023-09-15 13:43:50 +0300

    add EquBeam::run method

--- strength/src/equivalent_beam/equ_beam.rs
+++ strength/src/equivalent_beam/equ_beam.rs
@@ -1,9 +1,10 @@
-use super::cross_section::CrossSection;
 use std::collections::HashMap;
-
+use csv::Error as CSVError;
+use super::{cross_section::CrossSection, input::Input};
 
 
 pub struct EquBeam {
+    input: Input,
     cross_sections: HashMap<i32, CrossSection>,
 }
 
@@ -12,7 +13,12 @@ impl EquBeam {
         self.cross_sections.get(&id)
     }
 
-    fn new(cross_sections: HashMap<i32, CrossSection>) -> Self {
-        EquBeam { cross_sections }
+    pub fn new(input: Input, cross_sections: HashMap<i32, CrossSection>) -> Self {
+        EquBeam { input, cross_sections }
+    }
+
+    pub fn run(&mut self) -> Result<(), CSVError>{
+        self.cross_sections = self.input.run()?;
+        Ok(())
     }
 }
--- strength/src/main.rs
+++ strength/src/main.rs
@@ -1,10 +1,11 @@
-use equivalent_beam::input::Input;
-
+use equivalent_beam::{input::Input, equ_beam::EquBeam};
 mod equivalent_beam;
-
+use std::collections::HashMap;
 
 fn main() {
-    let input = Input::new("./equ_beam/equ_beam.csv".to_string());
-    input.run();
-
+    let mut equ_beam = EquBeam::new(
+        Input::new("./equ_beam/equ_beam.csv".to_string()),
+        HashMap::new(),
+    );
+    equ_beam.run().unwrap();
 }
