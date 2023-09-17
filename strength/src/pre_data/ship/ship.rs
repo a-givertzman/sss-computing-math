62ecf94703a590e1d17ea4c025f40470d554e225
milovanovmaksim <milovanov160386@gmail.com>
2023-09-17 23:48:59 +0300

    remove code in Ship::new() constructor

--- strength/src/pre_data/ship/ship.rs
+++ strength/src/pre_data/ship/ship.rs
@@ -10,28 +10,20 @@ pub struct Ship {
     
 }
 impl Ship {
-    pub fn new(file_path: String) -> Result<Self, String> {
-        match Ship::from_csv(&file_path) {
-            Ok(ship) => {
-                debug!("Ship::new | The ship hase been created successfully from file: {:#?}, Ship:\n{:#?}", file_path, ship);
-                Ok(ship)
-            }
-            Err(err) => {
-                warn!("Ship::new | error: {:?}",err);
-                Err(err.to_string())
-            }            
-        }
+    pub fn new(file_path: String) -> Self {
+        Ship { }
     }
 }
 
 impl FromCSV for Ship {
-    fn from_csv(file_path: &String) -> Result<Self, String> {
+    fn from_csv(file_path: String) -> Result<Self, String> {
         let input = Input::new(&file_path);
         match input.run() {
             Ok(mut parser) => {               
                 let mut result = parser.deserialize::<Ship>();
                 if let Some(record) = result.next() {
-                    let ship = record.map_err(|err| { err.to_string() })?;                 
+                    let ship = record.map_err(|err| { err.to_string() })?;
+                    debug!("Ship::new | The ship hase been created successfully from file: {:#?}, Ship:\n{:#?}", file_path, ship);               
                     return Ok(ship);
                 } else {
                     warn!("Ship::from_data | error: Expected one record but got none");
