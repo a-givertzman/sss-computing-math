7ab50c215324cedfb993d02f9e6784d7798d4ba2
milovanovmaksim <milovanov160386@gmail.com>
2023-09-18 13:55:39 +0300

    add constructor from_parser_csv

--- strength/src/pre_data/cross_section_properties/cross_sections.rs
+++ strength/src/pre_data/cross_section_properties/cross_sections.rs
@@ -1,51 +1,52 @@
 use std::collections::HashMap;
+use csv::Reader;
 use log::{debug, warn};
-use crate::pre_data::{from_csv::FromCSV, input::Input};
+use std::fs::File;
+use crate::pre_data::{from_csv::FromCSV, input::CSV};
 use super::cross_section::CrossSection;
+
 
 pub struct CrossSections {
     cross_sections: HashMap<i32, CrossSection>,
 }
 
 impl CrossSections {
-    pub fn new(file_path: String) -> Result<Self, String> {
-        match CrossSections::from_csv(&file_path) {
-            Ok(cross_sections) => {
-                debug!("CrossSections::new | CrossSections hase been created successfully from file: {:#?}, CrossSections:\n{:#?}", file_path, cross_sections.cross_sections);
-                Ok(cross_sections)
-            }
-            Err(err) => {
-                warn!("CrossSections::new | error: {:?}",err);
-                Err(err)
-
-            }
-        }
+    pub fn new(cross_sections: HashMap<i32, CrossSection>) -> Self {
+        CrossSections { cross_sections }
+    }
+    fn from_parser_csv(mut parser: Reader<File>) -> Result<Self, String> {
+        let mut cross_sections = HashMap::new();
+                for result in parser.deserialize::<CrossSection>() {
+                    match result {
+                        Ok(cross_section) => { cross_sections.insert(cross_section.id, cross_section); },
+                        Err(err) => {
+                            warn!("CrossSections::from_csv | error: {:?}",err);
+                            return Err(err.to_string());
+                        }
+                    }
+                }
+                if cross_sections.len() == 0 {
+                    warn!("CrossSections::from_data | error: Expected at least one record but got none.");
+                    return Err("Expected at least one record but got none.".to_owned());
+                }
+                debug!("CrossSections::from_csv | Cross sections have been created successfully.\n CrossSections:\n {:#?}", cross_sections);
+                Ok(CrossSections::new(cross_sections))
     }
 }
 
 
 impl FromCSV for CrossSections {
-    fn from_csv(file_path: &String) -> Result<Self, String> {
-        let input = Input::new(file_path);
-        match input.run() {
-            Ok(mut parser) => {
-                let mut cross_sections = HashMap::new();
-                for result in parser.deserialize::<CrossSection>() {
-                    let cross_section = result?;
-                    cross_sections.insert(cross_section.id, cross_section);
-                }
-                if cross_sections.len() == 0 {
-                    todo!("cros_sections не должен быть пустым")
-                }
-                debug!("CrossSections::from_csv | Cross sections have been applied successfully form file: {:#?} {:#?}", file_path, cross_sections);
-                Ok(CrossSections { cross_sections })
+    fn from_csv(file_path: String) -> Result<Self, String> {
+        let input = CSV::new(&file_path);
+        match input.parser() {
+            Ok(parser) => {
+                CrossSections::from_parser_csv(parser)
             },
             Err(err) => {
-                warn!("CrossSections::from_csv | error: {:?}",err);
-                Err(err)
+                warn!("CrossSections::from_csv | error: {:?}.",err);
+                Err(err.to_string())
             }
         }
-        
     }
 }
 
