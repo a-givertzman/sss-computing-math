e46bb20647aaae4df3887875312e1c1c5d692549
milovanovmaksim <milovanov160386@gmail.com>
2023-09-18 16:53:36 +0300

    add ShipMeasurements

--- strength/src/pre_data/ship/ship_measurements.rs
+++ strength/src/pre_data/ship/ship_measurements.rs
@@ -2,19 +2,22 @@ use serde::Deserialize;
 
 
 /// Ship measurements:
-/// - length_design_waterline - конструктивная ватерлиния,
+/// - length_design_waterline - длина по конструктивной ватерлинии,
+/// - width_design_waterline - ширина по конструктивной ватерлинии,
 /// - number_spatiums - количество теоретических шпаций,
+/// - draft - осадка,
 #[derive(Deserialize, Debug)]
 pub struct ShipMeasurements {
     length_design_waterline: f64,
+    width_design_waterline: f64,
     number_spatiums: i64,
 
 
 }
 
 impl ShipMeasurements {
-    pub fn new(length_design_waterline: f64, number_spatiums: i64,) -> Self {
-        ShipMeasurements { length_design_waterline, number_spatiums }
+    pub fn new(length_design_waterline: f64, number_spatiums: i64, width_design_waterline: f64) -> Self {
+        ShipMeasurements { length_design_waterline, number_spatiums, width_design_waterline }
     }
 
     pub fn length_spatium(&self) -> f64 {
