e747ec94970284abedda3826222c82cf7a55a373
milovanovmaksim <milovanov160386@gmail.com>
2023-09-21 13:09:38 +0300

    add Deadweight struct

---
+++ strength/src/pre_data/loads/deadweight/deadweight.rs
@@ -0,0 +1,38 @@
+use log::{debug, warn};
+use serde::Deserialize;
+
+use crate::{equivalent_beam::force::Force, pre_data::json::JSON};
+
+#[derive(Deserialize, Debug)]
+pub struct Deadweight {
+    loads: Vec<Force>,
+}
+
+
+impl Deadweight {
+    pub fn new(loads: Vec<Force>) -> Self {
+        Deadweight { loads }
+    }
+
+    pub fn from_json_file(file_path: String) -> Result<Self, String> {
+        let json = JSON::new(file_path);
+        match json.reader() {
+            Ok(reader) => {
+                match serde_json::from_reader(reader) {
+                    Ok(deadweight) => {
+                        debug!("Deadweight::from_json_file | Deadweight have been created from json file successfully.\n Deadweight:\n {:#?}", deadweight);
+                        return Ok(deadweight)
+                    },
+                    Err(err) => {
+                        warn!("Deadweight::from_json_file | error: {:?}.",err);
+                        return Err(err.to_string());
+                    }
+                }
+            },
+            Err(err) => {
+                warn!("Deadweight::from_json_file | error: {:?}.",err);
+                return Err(err.to_string());
+            }
+        }
+    }
+}
