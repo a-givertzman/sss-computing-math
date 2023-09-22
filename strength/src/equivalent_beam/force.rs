b189f73a8d35a0dc376c6def6a80ef31b32633f9
milovanovmaksim <milovanov160386@gmail.com>
2023-09-22 13:11:33 +0300

    change type for a field

--- strength/src/equivalent_beam/force.rs
+++ strength/src/equivalent_beam/force.rs
@@ -1,10 +1,10 @@
 use serde::Deserialize;
 
-use crate::equivalent_beam::{system_of_units::{Newton, Meters}, location::Location};
+use crate::equivalent_beam::{system_of_units::Newton, location::Location};
 
 #[derive(Deserialize, Debug)]
 pub struct Force {
     value: Newton,
     location: Location,
-    length: Meters,
+    length: f64,
 }
