c5f143a83a89791aa6f1cc47871203de47078642
milovanovmaksim <milovanov160386@gmail.com>
2023-09-22 18:41:05 +0300

    add spatiums method

--- strength/src/equivalent_beam/equ_beam.rs
+++ strength/src/equivalent_beam/equ_beam.rs
@@ -1,4 +1,7 @@
-use crate::pre_data::{cross_section_properties::cross_sections::CrossSections, ship::ship::Ship};
+use std::collections::HashMap;
+use log::warn;
+use crate::pre_data::{cross_section_properties::cross_sections::CrossSections, ship::{ship::Ship, loads::{type_load::TypeLoad, lightweight::lightweight::Lightweght, deadweight::deadweight::Deadweight}}};
+use super::spatium::Spatium;
 
 
 
@@ -14,7 +17,33 @@ impl EquBeam {
         EquBeam { cross_sections, ship }
     }
 
-    pub fn solve(&self) {
-        todo!()
+    pub fn spatiums(&self, type_load: TypeLoad) -> Result<HashMap<i64, Spatium>, String> {
+        match type_load {
+            TypeLoad::LightweghtIntensity => {
+                let lightweight = Lightweght::from_json_file("./pre_data/lightweight.json".to_string());
+                match lightweight {
+                    Ok(lightweight) => {
+                        return lightweight.spatiums();
+                    },
+                    Err(err) => {
+                        warn!("EquBeam.spatiums() | error: {:?}.",err);
+                        return Err(err.to_string());
+                    }
+                }
+            },
+            TypeLoad::DeadweightIntensity => {
+                match Deadweight::from_json_file("./pre_data/deadweight.json".to_string()) {
+                    Ok(deadweight) => { return deadweight.spatiums(); },
+                    Err(err) => {
+                        warn!("EquBeam.spatiums() | error: {:?}.",err);
+                        return Err(err.to_string());
+                    }
+                }
+            },
+            TypeLoad::DisplacementTonnageIntensity => { todo!() },
+            TypeLoad::BuoyantIntensity => todo!(),
+            TypeLoad::BendingMoment => todo!(),
+            TypeLoad::ShearForce => todo!(),
+        }
     }
 }
