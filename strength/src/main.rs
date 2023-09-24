0525c81ff543b83597653747acb6ec479bbb3807
milovanovmaksim <milovanov160386@gmail.com>
2023-09-25 00:00:45 +0300

    change main method

--- strength/src/main.rs
+++ strength/src/main.rs
@@ -1,6 +1,5 @@
 mod equivalent_beam;
 mod pre_data;
-use equivalent_beam::equ_beam::EquBeam;
 use pre_data::{cross_section_properties::cross_sections::CrossSections, ship::ship::Ship};
 use std::env;
 
@@ -8,10 +7,7 @@ fn main() {
     env::set_var("RUST_LOG", "debug");
     //env::set_var("RUST_BACKTRACE", "1");
     env::set_var("RUST_BACKTRACE", "full");
-    env_logger::init();
-
-    let cross_section = CrossSections::from_csv_file("./pre_data/cross_sections_data.csv".to_string());
-    let ship = Ship::from_json_file("./pre_data/ship_data.json".to_string()).unwrap();
+    env_logger::init();   
     
 }
 
