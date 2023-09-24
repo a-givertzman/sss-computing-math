ea9a7ddd0eaeef5fe096db79325e63ca12938adb
milovanovmaksim <milovanov160386@gmail.com>
2023-09-25 00:00:05 +0300

    delete spatiums method. Rename params method.

--- strength/src/pre_data/ship/loads/lightweight/lightweight.rs
+++ strength/src/pre_data/ship/loads/lightweight/lightweight.rs
@@ -1,22 +1,18 @@
-use std::collections::HashMap;
-
 use log::{warn, debug};
 use serde::Deserialize;
+use crate::{equivalent_beam::{system_of_units::Newton, spatium::Spatium}, pre_data::{ship::ship::Ship, json::JSON}};
 
-use crate::{equivalent_beam::{system_of_units::Tons, spatium::Spatium}, pre_data::{ship::ship::Ship, json::JSON}};
 
+/// Lightweight - the weight of the ship without cargo, fuel, lubricating oil, ballast water,
+/// fresh water and feed water in tanks, consumable stores, passengers and crew and their belongings.
 #[derive(Deserialize, Debug)]
-pub struct Lightweght {
-    massa: Tons,
+pub struct Lightweight {
+    weight: Newton,
 }
 
-impl Lightweght {
-    pub fn new(massa: Tons) -> Self {
-        Lightweght { massa }
-    }
-
-    pub fn from_f64(massa: f64) -> Self {
-        Lightweght::new(Tons(massa))
+impl Lightweight {
+    pub fn new(weight: Newton) -> Self {
+        Lightweight { weight }
     }
 
     pub fn from_json_file(file_path: String) -> Result<Self, String> {
@@ -25,62 +21,54 @@ impl Lightweght {
             Ok(reader) => {
                 match serde_json::from_reader(reader) {
                     Ok(ship) => {
-                        debug!("Lightweght::from_json_file | Lightweght have been created from json file successfully.\n Ship:\n {:#?}", ship);
+                        debug!("Lightweight::from_json_file | Lightweght have been created from json file successfully.\n Ship:\n {:#?}", ship);
                         return Ok(ship)
                     },
                     Err(err) => {
-                        warn!("Lightweght::from_json_file | error: {:?}.",err);
+                        warn!("Lightweight::from_json_file | error: {:?}.",err);
                         return Err(err.to_string());
                     }
                 }
             },
             Err(err) => {
-                warn!("Lightweght::from_json_file | error: {:?}.",err);
+                warn!("Lightweight::from_json_file | error: {:?}.",err);
                 return Err(err);
             }
         }
     }
-
-    pub fn spatiums(&self) -> Result<HashMap<i64, Spatium>, String> {
-         match Ship::from_json_file("./pre_data/ship_data.json".to_string()) {
+    /// Determines the lightweight intensity by spatiums
+    pub fn lightweight_intensity(&self) -> Result<Vec<Spatium>, String> {
+        match Ship::from_json_file("./pre_data/ship_data.json".to_string()) {
             Ok(ship) => {
-                let mut spatiums: HashMap<i64, Spatium> = HashMap::new();
-                let spatium_noce = Spatium::new(0, 20.0, ship.length_spatium());
-                let spatium_stern = Spatium::new(ship.number_spatiums(), 20.0, ship.length_spatium());
-                spatiums.insert(0, spatium_noce);
-                spatiums.insert(ship.number_spatiums(), spatium_stern);
-                let (a, b, c) = self.params(&ship);
-                let mut length = ship.length_spatium();
-                while length  < ship.length_design_waterline() - ship.length_spatium() {
-                    if length > 0.0 && length > ship.length_design_waterline() / 3.0 {
-
-                    }
-
-
-                    length += ship.length_spatium();
+                let mut spatiums = vec![];
+                let nose = ship.coord_nose();
+                let stern = ship.coord_stern();
+                let length_spatium = ship.length_spatium();
+                let mut current_coord = stern + length_spatium / 2.0;
+                let (a, b, c) = self.lightweight_intensity_parameters(&ship);
+                while current_coord < nose {
+                    let end_coord = current_coord + length_spatium / 2.0;
+                    let start_coord = current_coord - length_spatium / 2.0;
+                    
+                    let spatium = Spatium::new(start_coord, end_coord, 0.0, 0.0);
+                    spatiums.push(spatium);
+                    current_coord += length_spatium;
                 }
-
-                todo!()
+                Ok(spatiums)
             },
             Err(err) => {
-                warn!("Lightweght.spatiums() | error: {:?}.",err);
-                return Err(err.to_string());
+                warn!("Lightweight::from_json_file | error: {:?}.",err);
+                return Err(err);
             }
-        }
+        }               
     }
 
-    
-
-    
-    fn params(&self, ship: &Ship) -> (f64, f64, f64) {
+    fn lightweight_intensity_parameters(&self, ship: &Ship) -> (f64, f64, f64) {
         if ship.completeness_coefficient > 0.7 {
             (0.65, 1.20, 0.57)
         } else {
             (0.71, 1.17, 0.6)
         }
     }
-
-
-
 }
 
