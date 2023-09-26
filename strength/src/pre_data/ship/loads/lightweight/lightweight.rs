b0c878b79d5924a4a96a43b6748848e961b04d6f
milovanovmaksim <milovanov160386@gmail.com>
2023-09-26 16:40:25 +0300

    add spatium method

--- strength/src/pre_data/ship/loads/lightweight/lightweight.rs
+++ strength/src/pre_data/ship/loads/lightweight/lightweight.rs
@@ -22,7 +22,7 @@ impl Lightweight {
                 match serde_json::from_reader(reader) {
                     Ok(ship) => {
                         debug!("Lightweight::from_json_file | Lightweght have been created from json file successfully.\n Ship:\n {:#?}", ship);
-                        return Ok(ship)
+                        return Ok(ship);
                     },
                     Err(err) => {
                         warn!("Lightweight::from_json_file | error: {:?}.",err);
@@ -41,18 +41,12 @@ impl Lightweight {
         match Ship::from_json_file("./pre_data/ship_data.json".to_string()) {
             Ok(ship) => {
                 let mut spatiums = vec![];
-                let nose = ship.coord_nose();
-                let stern = ship.coord_stern();
-                let length_spatium = ship.length_spatium();
-                let mut current_coord = stern + length_spatium / 2.0;
-                let (a, b, c) = self.lightweight_intensity_parameters(&ship);
-                while current_coord < nose {
-                    let end_coord = current_coord + length_spatium / 2.0;
-                    let start_coord = current_coord - length_spatium / 2.0;
-                    
-                    let spatium = Spatium::new(start_coord, end_coord, 0.0, 0.0);
+                let mut current_coord = ship.coord_stern() + ship.length_spatium() / 2.0;
+                while current_coord <= (ship.coord_nose() - ship.length_spatium() / 2.0) {
+                    let spatium = self.spatium(current_coord, &ship);
+                    debug!("{:?}", spatium);
                     spatiums.push(spatium);
-                    current_coord += length_spatium;
+                    current_coord += ship.length_spatium();
                 }
                 Ok(spatiums)
             },
@@ -60,11 +54,33 @@ impl Lightweight {
                 warn!("Lightweight::from_json_file | error: {:?}.",err);
                 return Err(err);
             }
-        }               
+        }
     }
 
+    fn spatium(&self, current_coord: f64, ship: &Ship) -> Spatium {
+        let (a, b, c) = self.lightweight_intensity_parameters(&ship);
+        let end_coord = current_coord + ship.length_spatium() / 2.0;
+        let start_coord = current_coord - ship.length_spatium() / 2.0;
+        let intensity_load = |parametr: f64| {
+            ((self.weight.0 / ship.number_spatiums() as f64) * parametr) / ship.length_spatium()
+        };
+        if current_coord > ship.coord_stern() && current_coord < (ship.coord_stern() + ship.length_design_waterline() / 3.0) {
+            let parametr = a + ((b - a) * (ship.length_design_waterline() / 2.0 - current_coord.abs()))/(ship.length_design_waterline() / 3.0);
+            let intensity_load = intensity_load(parametr);
+            Spatium::new(start_coord, end_coord, intensity_load, intensity_load)
+        } else if current_coord >= ship.coord_stern() + ship.length_design_waterline() / 3.0 && current_coord < (ship.coord_nose() - ship.length_design_waterline() / 3.0) {
+            let intensity_load = intensity_load(b);
+            Spatium::new(start_coord, end_coord, intensity_load, intensity_load)
+        } else {
+            let parametr = c + ((b - c) * (ship.length_design_waterline() / 2.0 - current_coord))/(ship.length_design_waterline() / 3.0);
+            let intensity_load = intensity_load(parametr);
+            Spatium::new(start_coord, end_coord, intensity_load, intensity_load)
+        }
+    }
+
+
     fn lightweight_intensity_parameters(&self, ship: &Ship) -> (f64, f64, f64) {
-        if ship.completeness_coefficient > 0.7 {
+        if ship.completeness_coefficient  <= 0.7 {
             (0.65, 1.20, 0.57)
         } else {
             (0.71, 1.17, 0.6)
