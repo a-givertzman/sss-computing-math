1c6b553231be4af22529d39b9e0fddcc1fbde9d5
milovanovmaksim <milovanov160386@gmail.com>
2023-09-26 16:41:49 +0300

    change in main

--- strength/src/main.rs
+++ strength/src/main.rs
@@ -1,6 +1,6 @@
 mod equivalent_beam;
 mod pre_data;
-use pre_data::{cross_section_properties::cross_sections::CrossSections, ship::ship::Ship};
+use pre_data::{cross_section_properties::cross_sections::CrossSections, ship::{ship::Ship, loads::lightweight::lightweight::Lightweight}};
 use std::env;
 
 fn main() {
@@ -8,7 +8,9 @@ fn main() {
     //env::set_var("RUST_BACKTRACE", "1");
     env::set_var("RUST_BACKTRACE", "full");
     env_logger::init();   
-    
+    let lightweight = Lightweight::from_json_file("./pre_data/lightweight.json".to_string()).unwrap();
+    lightweight.lightweight_intensity();
+
 }
 
 
