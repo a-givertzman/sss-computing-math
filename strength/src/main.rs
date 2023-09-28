13bc79dce74048dca92c43044c141411ba4301fe
milovanovmaksim <milovanov160386@gmail.com>
2023-09-28 14:00:17 +0300

    add  some comments

--- strength/src/main.rs
+++ strength/src/main.rs
@@ -11,8 +11,25 @@ fn main() {
     env::set_var("RUST_LOG", "debug");
     //env::set_var("RUST_BACKTRACE", "1");
     env::set_var("RUST_BACKTRACE", "full");
-    env_logger::init();   
-    let lightweight = Lightweight::from_json_file("./pre_data/data.json".to_string()).unwrap();
+    env_logger::init();
+    let mut lightweight = Lightweight::from_json_file("./pre_data/data.json".to_string()).unwrap();
     lightweight.lightweight_intensity();
 
 }
+
+// Solution::new(
+//     EquBeam::new(
+//         CrossSections::from_csv_file(),
+//         BendingMoment::new(
+//             SheareForce::new(
+//                 TotalShipLoad::new(
+//                     BouyanLoad::new(Ship::from_json_file()),
+//                     Displacment::new(
+//                         Deadweight::from_json_file(),
+//                         Lightweight::from_json_file()
+//                     )
+//                 )
+//             )
+//         )
+//     )
+// ).soleve();
