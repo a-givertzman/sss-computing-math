8c9a11bee21728faf88c118a5ba981b8f01865d2
milovanovmaksim <milovanov160386@gmail.com>
2023-09-29 16:08:39 +0300

    add call_once

--- src/tests/unit/strength/lightweight/lightweight_test.rs
+++ src/tests/unit/strength/lightweight/lightweight_test.rs
@@ -1,17 +1,32 @@
 #[cfg(test)]
 mod tests {
+    use std::{sync::Once, env};
     use log::debug;
-
+    static INIT: Once = Once::new();
     use crate::strength::{ship::loads::lightweight::lightweight::Lightweight, output::type_output::TypeOutput};
+
+
+    fn call_once() {
+        INIT.call_once(|| {
+                env::set_var("RUST_LOG", "debug");  // off / error / warn / info / debug / trace
+                // env::set_var("RUST_BACKTRACE", "1");
+                env::set_var("RUST_BACKTRACE", "full");
+                env_logger::init();
+            }
+        )
+    }
+
 
     #[test]
     fn create_lightweight_from_json_file_successfully() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/correct_data.json".to_string());
         assert!(lightweight.is_ok());
     }
 
     #[test]
     fn create_lightweight_from_json_file_invalid_type() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/invalid_type.json".to_string());
         assert!(lightweight.is_err());
         assert!(lightweight.unwrap_err().contains("invalid type"));
@@ -19,12 +34,14 @@ mod tests {
 
     #[test]
     fn create_lightweight_from_json_file_missing_field() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/empty_field.json".to_string());
         assert!(lightweight.is_err());
         assert!(lightweight.unwrap_err().contains("missing field `lightweight"));
     }
     #[test]
     fn test_number_spatiums() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/correct_data.json".to_string()).unwrap();
         let output = lightweight.lightweight_intensity();
         let ship = lightweight.ship;
@@ -34,6 +51,7 @@ mod tests {
 
     #[test]
     fn test_type_output() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/correct_data.json".to_string()).unwrap();
         let output = lightweight.lightweight_intensity();
         assert_eq!(output.type_output, TypeOutput::LightweightIntensity);
@@ -41,6 +59,7 @@ mod tests {
 
     #[test]
     fn test_lightweight_intensity() {
+        call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/correct_data.json".to_string()).unwrap();
         let output = lightweight.lightweight_intensity();
         let mut weight = 0.0;
@@ -58,6 +77,8 @@ mod tests {
                 0.0
             }
         };
+        debug!("\nЭталонный вес корпуса корабля: {} [Ньютон]
+Вес корпуса корабля, полученный в результате численного интегрирования методом трапеций: {} [Ньютон]", lightweight.lightweight.0, weight);
         debug!("Ошибка численного интегрирования интенсивности веса корпуса корабля по его длине = {} %", err);
         assert!(err < 5.0, "Error more than 5% = {}%.", err);
     }
