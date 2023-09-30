d93f6477ec3bc797a5f83a37c42d0f546841090e
milovanovmaksim <milovanov160386@gmail.com>
2023-09-30 18:45:26 +0300

    implement draw method

--- src/strength/output/output.rs
+++ src/strength/output/output.rs
@@ -1,3 +1,5 @@
+use plotly::{Scatter, common::{Mode, Line, LineShape, Font, Title}, Plot, Layout, layout::{Legend, TraceOrder}};
+
 use crate::strength::ship::spatium::Spatium;
 
 use super::type_output::TypeOutput;
@@ -16,7 +18,26 @@ impl Output {
     }
     
     pub fn draw(&self) {
-        todo!()
+        let mut x = vec![];
+        let mut y = vec![];
+        for spatiun in &self.spatiums {
+            x.push(spatiun.start_coord);
+            y.push(spatiun.start_value);
+            x.push(spatiun.end_coord);
+            y.push(spatiun.end_value);
+        }
+        let step = (self.spatiums[0].end_coord - self.spatiums[0].end_coord).abs();
+        let trace1 = Scatter::new(x, y)
+        .mode(Mode::LinesMarkers)
+        .name("name")
+        .line(Line::new().shape(LineShape::Linear));  
+        let mut plot = Plot::new();
+        let layout = Layout::new()
+            .legend(Legend::new().font(Font::new().size(16)))
+            .title(Title::new("Lightweight intensity"));
+        plot.add_trace(trace1);
+        plot.set_layout(layout);
+        plot.show();
     }
 }
 
