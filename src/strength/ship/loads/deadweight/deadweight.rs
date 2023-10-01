96d581bf21c0a386ff1ef0fc34fcf703e5410399
milovanovmaksim <milovanov160386@gmail.com>
2023-10-01 12:48:57 +0300

    add todo! in some methods

--- src/strength/ship/loads/deadweight/deadweight.rs
+++ src/strength/ship/loads/deadweight/deadweight.rs
@@ -19,25 +19,7 @@ impl Deadweight {
     }
 
     pub fn from_json_file(file_path: String) -> Result<Self, String> {
-        let json = JSON::new(file_path);
-        match json.reader() {
-            Ok(reader) => {
-                match serde_json::from_reader(reader) {
-                    Ok(deadweight) => {
-                        debug!("Deadweight::from_json_file | Deadweight have been created from json file successfully.\n Deadweight:\n {:#?}", deadweight);
-                        return Ok(deadweight)
-                    },
-                    Err(err) => {
-                        warn!("Deadweight::from_json_file | error: {:?}.",err);
-                        return Err(err.to_string());
-                    }
-                }
-            },
-            Err(err) => {
-                warn!("Deadweight::from_json_file | error: {:?}.",err);
-                return Err(err);
-            }
-        }
+        todo!()
     }
 
     pub fn deadweight_intensity(&self, ship: &Ship) -> Vec<Spatium> {
