99df901cf3d777e17edc1589fa717b49fdbba91f
milovanovmaksim <milovanov160386@gmail.com>
2023-10-16 15:05:01 +0300

    add a doc string for the test

--- src/tests/unit/strength/lightweight/lightweight_test.rs
+++ src/tests/unit/strength/lightweight/lightweight_test.rs
@@ -58,6 +58,8 @@ mod tests {
         assert_eq!(output.type_output, TypeOutput::LightweightIntensity);
     }
 
+    ///
+    /// Lightweight расчитанное, не должно отличаться от заданного более чем на 0.5% [https://github.com/a-givertzman/sss-computing-math/blob/Documentation-%7C-collect-all-researches-in-the-/design/design/strength/%D0%A0%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B9%20%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BF%D1%83%D1%81%D0%B0%20%D0%BA%D0%BE%D1%80%D0%B0%D0%B1%D0%BB%D1%8F.pdf].
     #[test]
     fn test_lightweight_intensity() {
         call_once();
@@ -78,7 +80,6 @@ mod tests {
             }
         };
         debug!("\nОтносительная ошибка численного интегрирования интенсивности веса корпуса корабля = {} %", err);
-        // Lightweight расчитанное, не должно отличаться от заданного более чем на 0.5%.
         assert!(err < 0.5, "Error more than 0.5% = {}%.", err);
     }
 }
