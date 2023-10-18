8aecca6ce9e29fbffd7e6983121e2f8a6e2e8af2
milovanovmaksim <milovanov160386@gmail.com>
2023-10-18 16:59:36 +0300

    delete old implementation of Load class

--- src/strength/ship/loads/load.rs
+++ src/strength/ship/loads/load.rs
@@ -1,43 +1,8 @@
-use log::{warn, debug};
 use serde::Deserialize;
-use crate::{core::json_file::JsonFile, strength::output::{type_output::TypeOutput, output::Output}};
-
-use super::{lightweight::lightweight::Lightweight, deadweight::deadweight::Deadweight};
 
 
 #[derive(Deserialize, Debug)]
 pub struct Load {
-    lightweight: Lightweight,
-    deadweight: Deadweight
 }
 
 
-impl Load {
-    pub fn new(lightweight: Lightweight, deadweight: Deadweight) -> Self {
-        Load { lightweight, deadweight }
-    }
-
-
-    pub fn from_json_file(file_path: String) -> Result<Self, String> {
-        todo!()
-    }
-
-    pub fn spatiums(&self, type_ouput: TypeOutput) -> Output {
-        match type_ouput {
-            TypeOutput::LightweightIntensity => {
-                self.lightweight.lightweight_intensity()
-            },
-            TypeOutput::DeadweightIntensity => {
-                todo!()
-            },
-            TypeOutput::DisplacementIntensity => { todo!() },
-            TypeOutput::BuoyantIntensity => todo!(),
-            TypeOutput::TotalLoadIntensity => todo!(),
-            TypeOutput::ShearForce => todo!(),
-            TypeOutput::BendingMoment => todo!(),
-            TypeOutput::Stress => todo!(),
-        }
-    }
-
-
-}
